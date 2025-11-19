5bb87dd34ee88a655a55d2e9bce1baa9e7b83fbc
Collin <collin.smith@7summitsinc.com>
2025-11-19 08:24:35 -0600

    fixes table after migration

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,23 +1,38 @@
 import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt";
 
-export async function getUsers(req, res) {
+export async function createUser(req, res, next) {
   try {
-    const users = await prisma.user.findMany();
-    res.json(users);
+    const { email, name, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(422).json({ message: "Email and password are required" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+
+    // Don't return the password in the response
+    const { password: _, ...userWithoutPassword } = user;
+
+    res.status(201).json(userWithoutPassword);
   } catch (err) {
     next(err);
   }
 }
 
-export async function createUser(req, res) {
+export async function getUsers(req, res) {
   try {
-    const { email, name } = req.body;
-
-    const user = await prisma.user.create({
-      data: { email, name },
-    });
-
-    res.status(201).json(user);
+    const users = await prisma.user.findMany();
+    res.json(users);
   } catch (err) {
     next(err);
   }
