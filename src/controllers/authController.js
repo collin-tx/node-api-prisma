3dc8259779e046c8c283c9dfc656dd7134b787c2
Collin <collin.smith@7summitsinc.com>
2025-11-20 11:55:43 -0600

    adds Login + JWT Authentication

---
+++ src/controllers/authController.js
@@ -0,0 +1,39 @@
+import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import ApiError from "../lib/ApiError.js";
+
+export async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new ApiError("Email and password are required", 422);
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new ApiError("Invalid email or password", 401);
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new ApiError("Invalid email or password", 401);
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    next(err);
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { getUsers, createUser } from "../controllers/userController.js";
+import { login } from "../controllers/authController.js";
 
 const router = Router();
 
 router.get("/users", getUsers);
-
+router.post("/users", createUser);
+router.post("/login", login);
 router.get("/", (req, res) => {
     res.send("Welcome to Node API Starter!");
 });
 
-router.post("/users", createUser);
 
 export default router;
